323f3723698d70642502449e3fff5e4d005cbe5d
DavidBonelo <bonelodev@gmail.com>
2023-04-19 12:58:17 -0500

    implement author, genre and bookinstance UPDATE

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -154,11 +154,68 @@ exports.genre_delete_post = (req, res, next) => {
 };
 
 // Display Genre update form on GET.
-exports.genre_update_get = (req, res) => {
-  res.send("NOT IMPLEMENTED: Genre update GET");
+exports.genre_update_get = (req, res, next) => {
+  Genre.findById(req.params.id).exec(function (err, genre) {
+    if (err) {
+      return next(err);
+    }
+    if (genre == null) {
+      // No results.
+      const err = new Error("Genre not found");
+      err.status = 404;
+      return next(err);
+    }
+
+    res.render("genre_form", {
+      title: 'Update Genre',
+      genre: genre,
+    });
+  });
 };
 
 // Handle Genre update on POST.
-exports.genre_update_post = (req, res) => {
-  res.send("NOT IMPLEMENTED: Genre update POST");
-};
+exports.genre_update_post = [
+  // Validate and sanitize the name field.
+  body("name", "Genre name required").trim().isLength({ min: 1 }).escape(),
+
+  // Process request after validation and sanitization.
+  (req, res, next) => {
+    // Extract the validation errors from a request.
+    const errors = validationResult(req).array();
+
+    // Create a genre object with escaped and trimmed data.
+    const genre = new Genre({
+      name: req.body.name,
+      _id: req.params.id, //This is required, or a new ID will be assigned!
+    });
+
+    // Check if Genre with same name already exists.
+    Genre.findOne({ name: req.body.name }).exec((err, found_genre) => {
+      if (err) {
+        return next(err);
+      }
+      if (found_genre) {
+        // Genre exists, add error
+        errors.push("A genre with the same name already exists");
+      }
+    });
+
+    if (errors.length!=0) {
+      // There are errors. Render the form again with sanitized values/error messages.
+      res.render("genre_form", {
+        title: "Update Genre",
+        genre,
+        errors: errors,
+      });
+      return;
+    }
+    // Data from form is valid. Update the record.
+    Genre.findByIdAndUpdate(req.params.id, genre, {}, (err, thegenre) => {
+      if (err) {
+        return next(err);
+      }
+      // Successful: Redirect to genre detail page.
+      res.redirect(thegenre.url);
+    });
+  },
+];
